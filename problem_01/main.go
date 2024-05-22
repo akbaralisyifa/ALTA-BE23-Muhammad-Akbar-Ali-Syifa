c2c1dd22b661ed3275955b30ef70398eda59c19e
akbaralisyifa <akbaralisyifa22@gmail.com>
2024-05-22 09:43:30 +0700

    task(problem 1): play with asterik

--- problem_01/main.go
+++ problem_01/main.go
@@ -2,25 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
-	var name string
-	var nilai int
+func playWithAsterik(n int) string {
 
-	fmt.Println("Input your name: ")
-	fmt.Scanln(&name)
-	fmt.Println("Input your nilai: ")
-	fmt.Scanln(&nilai)
-
-	switch {
-		case (nilai >= 80):
-			fmt.Println("nama:", name, ", Nilai: A")
-		case (nilai >= 65):
-			fmt.Println("nama:", name, ", Nilai: B+")
-		case(nilai >= 50):
-			fmt.Println("nama:", name, ", Nilai: B")
-		case(nilai >= 35):
-			fmt.Println("nama:", name, ", Nilai: C")
-		default:
-			fmt.Println("nama:", name, ", Nilai: D")
+	for i := 1; i <= n; i++ {
+		fmt.Print("\n")
+		for s := 1; s <= n - i; s++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= i + (i - 1); j++ {
+			fmt.Print("*")
+		}
 	}
+	return ""
 }
+
+	func main(){
+		playWithAsterik(5)
+	}
