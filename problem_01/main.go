111d45123082f1129d06feef5807d0683cc672d7
akbaralisyifa <akbaralisyifa22@gmail.com>
2024-06-04 13:06:06 +0700

    task(problem 1): simple equations

--- problem_01/main.go
+++ problem_01/main.go
@@ -2,13 +2,27 @@ package main
 
 import "fmt"
 
-func swap(a, b *int) {
-	*a, *b = *b, *a
+func sampleEquations(a, b, c int) {
+	found := false
+
+	for x:= -100; x <= 100; x++{
+		for y:= -100; y <= 100; y++{
+			for z:= -100; z <= 100; z++{
+				if x+y+z == a && x*y*z == b && x*x+y*y+z*z == c{
+					fmt.Printf("Output: %d %d %d\n", x, y, z)
+					found = true
+					return
+				}
+			}
+		}
+	}
+
+	if !found {
+		fmt.Println("no solution")
+	}
 }
 
 func main() {
-	a := 10
-	b := 20
-	swap(&a, &b)
-	fmt.Println(a, b)
+	sampleEquations(1, 2, 3) // no solution
+	sampleEquations(6, 6, 14)
 }
